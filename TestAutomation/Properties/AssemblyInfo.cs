d042af1ab990dce2663e129d646b9d12c4cee6a0
Esteban Sosa <esosa2@dxc.com>
2021-06-14 11:27:05 -0600

    new files

---
+++ TestAutomation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAutomation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestAutomation")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a35b4df8-ead2-41db-a1c0-b1f6e13d049f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestAutomation/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestCodeChallange;
+
+namespace TestAutomation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+           tes
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
